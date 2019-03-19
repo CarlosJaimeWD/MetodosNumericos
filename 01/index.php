320bb1c7001ee3347e14cdfe24133acbfb4441ce
CarlosJaimeWD <carlosjaimewd@gmail.com>
2019-03-19 17:44:00 -0600

    Update

--- 01/index.php
+++ 01/index.php
@@ -7,9 +7,10 @@
     <title>Metodos Numericos 01</title>
     <script src="Js/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="Css/Style.css">
+    <link rel="stylesheet" href="Css/Background.css">
 </head>
 <body>
-    <main class="main">
+    <!--<main class="main">
         <label for="">Valor C</label>
         <input type="number" id="numberToSearch" value="27">
         <label for="">Valor Inicial</label>
@@ -19,35 +20,58 @@
         <label for="">% Error</label>
         <input type="number" id="epsilon" value=".005">
         <button type="submit" id="submit">Calcular</button>
+    </main>-->
+
+    <!-- Background elements -->
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+    <div id="horizon">
+        <div class="glow"></div>
+    </div>
+    <div id="earth"></div>
+    <div id="title">Metodos Numericos</div>
+    <div id="subtitle">
+        <span>By</span>
+        <span>Carlos</span>
+        <span>Jaime</span>
+    </div>
+
+    <main id="globalContainer">
+        <div id="mainContainer">
+            <div id="titleBox">Metodos Numericos - </div>
+            <div id="formBox"></div>
+        </div>
     </main>
 
     <script>
         $(document).ready(function() {
-            console.log("Hola");
+             setTimeout(function(){ 
+                $("#title").hide();
+                $("#subtitle").hide();
+             }, 5500);
+
+             setTimeout(function(){ 
+                $("#earth").hide();
+                $("#horizon").hide();
+             }, 8150);
+
+             setTimeout(function(){ 
+                $("#mainContainer").animate({
+                    width: '65%',
+                    height: '60%'
+                }, 'slow');
+             }, 6700);
         });
 
         $("#submit").click(function() {
             var numberToSearch = $("#numberToSearch").val();
             var epsilon = $("#epsilon").val();
             var numberInitial = $("#numberInitial").val();
-            var x = (1/2) * (parseInt(numberInitial) + (parseInt(numberToSearch)/parseInt(numberInitial)));
-            var errorRelativo = Math.abs((parseFloat(x) - parseFloat(numberInitial)) / x);
             
-            console.log("numberToSearch: " + numberToSearch + ", numberInitial: " + numberInitial + ", epsilon: " + epsilon);
-            console.log("x: " + x + ", errorRelativo: " + errorRelativo);
             
+            //console.log("numberToSearch: " + numberToSearch + ", numberInitial: " + numberInitial + ", epsilon: " + epsilon);            
         });
     </script>
-
-    <?php
-
-        $x = 35;
-        $y = 40;
-
-        $z = $x+$y;
-
-        echo $z;
-
-    ?>
 </body>
 </html>
